4b54d38d90e0302be05f40a3266d9424e8898947
rahma717 <rahmabouabid2@gmail.com>
2024-03-06 22:23:47 +0100

    Correction of the date, the white page, the dots on the slider, and the logo

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
+
 
 import "./style.scss";
 
@@ -12,17 +13,20 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
-    nextCard();
+    if (data) {
+      nextCard();
+    } 
+    
   });
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <React.Fragment key={event.description}>
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
@@ -41,16 +45,16 @@ const Slider = () => {
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
+                <input readOnly
+                  key={`${_.date}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
